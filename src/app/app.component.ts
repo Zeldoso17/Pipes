10f01f709fa52938a6d86e05c147588eefd5eabe
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-09-28 21:56:32 -0700

    Proyecto con Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  description = 'Este es una aplicacion para probar los "pipes"'
+  nombreMaestro = 'melchor carranza santana'
+  frutas = ['Manzana', 'Durazno', 'Pera', 'Fresa', 'Uva', 'Platano', 'Mora', 'Mango']
+  PI = Math.PI
+  porcentaje = 0.6048
+  persona = [
+    {
+      nombre: 'Ernesto',
+      edad: 32
+    },
+    {
+      nombre: 'Maria',
+      edad: 25
+    },
+    {
+      nombre: 'Marlene',
+      edad: 18
+    }
+  ]
+  fecha = Date.now()
+  dinero = 100.568
+  time = new Observable<string>(observer => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+
 }
